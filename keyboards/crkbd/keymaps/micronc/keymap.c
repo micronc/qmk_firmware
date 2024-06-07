66f435615e2d18191d3b35067fee403bd7181d5c
micronc <micronc@users.noreply.github.com>
2024-06-06 23:08:03 -0400

    Update keymap.c
    Still needs more work, but this was done to match my voyager

--- keyboards/crkbd/keymaps/micronc/keymap.c
+++ keyboards/crkbd/keymaps/micronc/keymap.c
@@ -27,32 +27,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, KC_LGUI,  MO(1),      KC_SPC,   MO(2), KC_RALT
+                                          KC_LCTL, MO(1), KC_SPC,     KC_SPC,   MO(2), KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_1,    KC_2,    KC_3, KC_LPRN, KC_RPRN, KC_BSPC,
+      KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_7,    KC_8,    KC_9, KC_LPRN, KC_RPRN, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX,                       KC_4,    KC_5,    KC_6, KC_RBRC, KC_BSLS,  KC_GRV,
+      KC_LCTL, KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX,                       KC_4,    KC_5,    KC_6, KC_RBRC, KC_BSLS,  KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_MINS,  KC_EQL, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_7,    KC_8,    KC_9, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_LSFT, KC_MINS,  KC_EQL, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_1,    KC_2,    KC_3, KC_RCBR, KC_PIPE, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(3),  KC_SPC,     KC_ENT,  KC_RALT, KC_0
+                                          KC_LGUI,   XXXXXXX,  KC_SPC,     KC_ENT,  KC_0, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
+       KC_ESC, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC,                      XXXXXXX, KC_HOME,   KC_UP,  KC_END, XXXXXXX, KC_PGUP,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, KC_PGDN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, _______,  KC_SPC,     KC_ENT,   MO(3), KC_RALT
+                                          KC_LGUI, MO(3),  KC_SPC,     KC_ENT,   XXXXXXX, KC_RALT
                                       //`--------------------------'  `--------------------------'
   ),
 
