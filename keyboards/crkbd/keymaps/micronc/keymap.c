ee9e50cd129724c0a57f59d550be698b2988a630
micronc <micronc@users.noreply.github.com>
2024-06-14 21:56:25 -0400

    keymap.c
    Made match as best I could for my voyager.

--- keyboards/crkbd/keymaps/micronc/keymap.c
+++ keyboards/crkbd/keymaps/micronc/keymap.c
@@ -27,18 +27,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL, MO(1), KC_SPC,     KC_SPC,   MO(2), KC_RALT
+                                          KC_LCTL, MO(1), KC_SPC,      KC_SPC,   MO(2), KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_7,    KC_8,    KC_9, KC_LPRN, KC_RPRN, KC_DEL,
+      KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_7,    KC_8,    KC_9, KC_PMNS , KC_DOT, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX,                       KC_4,    KC_5,    KC_6, KC_RBRC, KC_BSLS,  KC_PIPE,
+      KC_LCTL, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                       KC_4,    KC_5,    KC_6, KC_PPLS, KC_BSLS,  KC_PIPE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_MINS,  KC_EQL, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_1,    KC_2,    KC_3, KC_RCBR, KC_PIPE, XXXXXXX,
+      KC_LSFT, KC_MINS,  KC_EQL, XXXXXXX, KC_LCBR, KC_RCBR,                       KC_1,    KC_2,    KC_3, KC_DOT, KC_EQL, KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   XXXXXXX,  KC_SPC,     KC_ENT,  KC_0, KC_RALT
                                       //`--------------------------'  `--------------------------'
